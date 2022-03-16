175790251f81ab2f10045003605d789db2af0861
Ylana Portela <ylanaportela2000@gmail.com>
2022-03-15 23:14:16 -0300

    Add header component

---
+++ components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import header from './header.module.scss'
+
+export default function Header(){
+    return(
+        <div className={header.title}>
+           <h1>Olá</h1>
+        </div>
+    )
+}
