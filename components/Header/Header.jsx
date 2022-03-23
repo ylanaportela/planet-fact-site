7ec59154a0efbf5084769101a9e9b8b517132b64
Ylana Portela <ylanaportela2000@gmail.com>
2022-03-22 22:13:37 -0300

    Add component header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,9 +1,26 @@
 import header from './header.module.scss'
+import Link from 'next/link'
+import Datas from '../../data.json'
 
 export default function Header(){
     return(
-        <div className={header.title}>
-           <h1>Olá</h1>
-        </div>
+        <header>
+            <div className={header.title}>
+                <h1>THE PLANETS</h1>
+            </div>
+
+            <ul>
+            { Datas.map(data =>{
+                return(
+                    <li key={data.name}>
+                        <Link href={`/${data.name}`}>
+                            <a>{data.name}</a>
+                        </Link>
+                    </li>
+            )})
+            }
+               
+            </ul>
+        </header>
     )
 }
