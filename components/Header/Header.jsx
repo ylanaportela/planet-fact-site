d6ef2382f283a5838837b9fc61262920e50a9486
Ylana Portela <ylanaportela2000@gmail.com>
2022-03-28 23:38:09 -0300

    build menu responsive

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,29 +3,34 @@ import Link from 'next/link'
 import Datas from '../../data.json'
 
 
-
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <header className={header.container}>
-            <div className={header.title}>
-                <h1>THE PLANETS</h1>
+            <div className={header.menu}>
+                <div className={header.title}>
+                    <h1>THE PLANETS</h1>
+                </div>
+                <div>
+                    <button className={header.button}><img src='/assets/icon-hamburger.svg' alt='Icon of menu hamburguer' /></button>
+                </div>
             </div>
-            <div>
-                <img src='/assets/icon-hamburger.svg' alt='Icon of menu hamburguer'/>  
-            </div>
+            
+            <nav  className={header.nav}>
+                <ul className={header.list}>
+                    {Datas.map(data => {
+                        return (
+                            <li key={data.name} className={header.card}>
 
-            <ul>
-            { Datas.map(data =>{
-                return(
-                    <li key={data.name}>
-                        <Link href={`/${data.name}`}>
-                            <a>{data.name}</a>
-                        </Link>
-                    </li>
-            )})
-            }
-               
-            </ul>
+                                <Link href={`/${data.name}`}>
+                                    <a className={header.link}>{data.name.toUpperCase()}</a>
+                                </Link>
+                                <img src='/assets/icon-chevron.svg'/>
+                            </li>
+                        )
+                    })
+                    }
+                </ul>
+            </nav>
         </header>
     )
 }
