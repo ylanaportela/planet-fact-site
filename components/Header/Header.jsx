36fd3e7fe40d42c07a95cccdea2ade78bf481e87
Ylana Portela <ylanaportela2000@gmail.com>
2022-03-30 23:38:35 -0300

    finalizing header menu and add icon component

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -2,39 +2,76 @@ import header from './header.module.scss'
 import Link from 'next/link'
 import Datas from '../../data.json'
 import Icon from '../Icon'
-
+import { useEffect, useState } from 'react'
+import IconMenu from '../IconMenu'
 
 export default function Header() {
+
+    const [ activeMenu, setActiveMenu ] = useState(false)
+    const [width, setWidth] = useState(0)
+
+    useEffect(()=>{
+        setWidth(window.innerWidth)
+    })
+
+    function handleClick(){
+        activeMenu == false ? setActiveMenu(true) : setActiveMenu(false)
+    }
+
     return (
         <header className={header.container}>
+
             <div className={header.menu}>
+
                 <div className={header.title}>
                     <h1>THE PLANETS</h1>
                 </div>
+
                 <div>
-                    <button className={header.button}><img src='/assets/icon-hamburger.svg' alt='Icon of menu hamburguer' /></button>
+                    <button 
+                    className={header.button}
+                    onClick={handleClick}>
+                        <IconMenu name={activeMenu === false ? header.buttonDefault : header.buttonActive}/>
+                    </button>
                 </div>
+
             </div>
+
+
+            {   
+               width == 375 && activeMenu == false ? 
+                    <div className={header.selection}>
+                        <ul className={header.listSelection}>
+                            <li>OVERVIEW</li>
+                            <li>STRUCTURE</li>
+                            <li>SURFACE</li>
+                        </ul>
+                    </div>
+                : 
+                    <nav  className={header.nav}>
+
+                        <ul className={header.list}>
+                            {Datas.map(data => {
+                                return (
+                                    <li key={data.name} className={header.item}>
+
+                                        <Icon name={`${header.oval} ${data.name.toLowerCase()}`} />
+
+                                        <Link href={`/${data.name}`}>
+                                            <a className={header.link}>{data.name.toUpperCase()}</a>
+                                        </Link>
+                                        
+                                        <img src='/assets/icon-chevron.svg'/>
+                                    </li>
+                                )
+                            })
+                            }
+                        </ul>
+
+                    </nav> 
+
+            }
             
-            <nav  className={header.nav}>
-                <ul className={header.list}>
-                    {Datas.map(data => {
-                        return (
-                            <li key={data.name} className={header.card}>
-
-                                <Icon name={`${header.oval} ${data.name.toLowerCase()}`} />
-
-                                <Link href={`/${data.name}`}>
-                                    <a className={header.link}>{data.name.toUpperCase()}</a>
-                                </Link>
-                                
-                                <img src='/assets/icon-chevron.svg'/>
-                            </li>
-                        )
-                    })
-                    }
-                </ul>
-            </nav>
         </header>
     )
 }
---
+++ components/IconMenu.jsx
@@ -0,0 +1,10 @@
+export default function IconMenu(props){
+    return(
+        <>
+            <svg className={props.name} xmlns="http://www.w3.org/2000/svg" width="24" height="17" fill="currentColor">
+                <g fillRule="evenodd"><path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z"/></g>
+            </svg>
+        </>
+    )
+}
+
